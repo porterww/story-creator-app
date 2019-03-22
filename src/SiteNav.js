9dbaa68733a64e7ae81f3b6e5bb2f9304e8839f2
Porter Webster <porterwwebster@gmail.com>
2019-03-21 20:41:15 -0600

    Added new render functions for the pages to compliment the new UserNav and it's pages. Added EditPage and ViewPAge imports. Updates the nav parent with a className for new CSS uses. Tweaked Link components.

--- src/SiteNav.js
+++ src/SiteNav.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import EditPage from './EditPage'
+import ViewPage from './ViewPage'
 
 class SiteNav extends Component {
+
+  renderEditor = () => {
+    return <EditPage />
+  }
+  renderViewer = () => {
+    return <ViewPage />
+  }
+
   render() {
     return (
-      <nav>
-        <Link to="/">Editor</Link>
+      <nav className="storyNav">
+        <Link to="/Edit-Page" component={this.renderEditor}>
+          Editor
+        </Link>
         <div className="header-split" />
-        <Link to="/ViewPage">Viewer</Link>
+        <Link to="/View-Page" component={this.renderViewer}>
+          Viewer
+        </Link>
       </nav>
     )
   }
